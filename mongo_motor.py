c6884d51c500be3a55a1d3dbf0a461281fb0208c
SmiteLi <user.email>
2020-04-04 22:25:47 +0800

    learn mongo

--- mongo_motor.py
+++
@@ -1,6 +0,0 @@
-# $ pip install motor
-import motor
-
-client = client = motor.motor_tornado.MotorClient(
-    "mongodb+srv://<username>:<password>@<cluster-url>/test?retryWrites=true&w=majority")
-db = client.test
--- mongopy.py
+++
@@ -1,5 +0,0 @@
-import pymongo
-
-client = pymongo.MongoClient(
-    "mongodb+srv://<username>:<password>@<cluster-url>/test?retryWrites=true&w=majority")
-db = client.test
